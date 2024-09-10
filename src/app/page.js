55718964d088d99d63555a575eb7dc7f88fe2711
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2024-09-10 13:38:05 +0900

    now quarternion OK

--- src/app/page.js
+++ src/app/page.js
@@ -35,6 +35,14 @@ export default function Home() {
   const [j6_pos,set_j6_pos] = React.useState({x:0,y:0,z:0})
   const [j6_rot,set_j6_rot] = React.useState({x:0,y:0,z:0})
 
+  const [j1_q,set_j1_q] = React.useState({x:0,y:0,z:0,w:1})
+  const [j2_q,set_j2_q] = React.useState({x:0,y:0,z:0,w:1})
+  const [j3_q,set_j3_q] = React.useState({x:0,y:0,z:0,w:1})
+  const [j4_q,set_j4_q] = React.useState({x:0,y:0,z:0,w:1})
+  const [j5_q,set_j5_q] = React.useState({x:0,y:0,z:0,w:1})
+  const [j6_q,set_j6_q] = React.useState({x:0,y:0,z:0,w:1})
+
+
   const [c_pos_x,set_c_pos_x] = React.useState(0)
   const [c_pos_y,set_c_pos_y] = React.useState(0.6)
   const [c_pos_z,set_c_pos_z] = React.useState(1.0)
@@ -63,11 +71,8 @@ export default function Home() {
     if (j1_object !== undefined) {
       const yAxis = new THREE.Vector3(0,1,0);
       j1_object.quaternion.setFromAxisAngle(yAxis,j1_rotate*(Math.PI/180))
-      console.log("j1_obj",j1_object.quaternion)
-      const oj1 = document.getElementById('oj1').object3D
-      console.log("j1_fig",oj1.quaternion)
-      const cj1 = document.getElementById('cj1').object3D
-      console.log("j1_cur",cj1.quaternion)
+//      const oj1 = document.getElementById('oj1').object3D
+//      const cj1 = document.getElementById('cj1').object3D
 
     }
   }, [j1_rotate])
@@ -95,7 +100,7 @@ export default function Home() {
 
   React.useEffect(() => {
     if (j5_object !== undefined) {
-      const yAxis = new THREE.Vector3(1,0,0);
+      const yAxis = new THREE.Vector3(0,1,0);
       j5_object.quaternion.setFromAxisAngle(yAxis,j5_rotate*(Math.PI/180))
     }
   }, [j5_rotate])
@@ -141,21 +146,30 @@ export default function Home() {
       const j1_wkq = new THREE.Quaternion();
       j1_wkq.copy(j1_object.quaternion).normalize()
       const j1_rot = getquaternion_Euler(j1_wkq)
+      set_j1_q({x:j1_wkq.x,y:j1_wkq.y,z:j1_wkq.z,w:j1_wkq.w})
       const j1_pos = getpos(j1_result.position)
       set_j1_pos(j1_pos)
       set_j1_rot(j1_rot)
 
+      // やりたいことは、グローバル座標における J2 姿勢の取得
+      // J2 の姿勢は、J1 の姿勢に J2 の回転を掛けたもの
       const j2_result = getposq(j2_object)
       const j2_wkq = new THREE.Quaternion();
-      j2_wkq.multiplyQuaternions(j2_object.quaternion,j1_wkq).normalize()
-      const j2_rot = getquaternion_Euler(j2_wkq)
+      // これ逆じゃない？
+//      j2_wkq.multiplyQuaternions(j2_object.quaternion,j1_wkq).normalize()
+      j2_wkq.multiplyQuaternions(j1_wkq,j2_object.quaternion).normalize()
+
+      set_j2_q({x:j2_wkq.x,y:j2_wkq.y,z:j2_wkq.z,w:j2_wkq.w}) 
       const j2_pos = getpos(j2_result.position)
       set_j2_pos(j2_pos)
       set_j2_rot(j2_rot)
 
       const j3_result = getposq(j3_object)
       const j3_wkq = new THREE.Quaternion();
-      j3_wkq.multiplyQuaternions(j3_object.quaternion,j2_wkq).normalize()
+//      j3_wkq.multiplyQuaternions(j3_object.quaternion,j2_wkq).normalize()
+      j3_wkq.multiplyQuaternions(j2_wkq,j3_object.quaternion).normalize()
+      set_j3_q({x:j3_wkq.x,y:j3_wkq.y,z:j3_wkq.z,w:j3_wkq.w})
+
       const j3_rot = getquaternion_Euler(j3_wkq)
       const j3_pos = getpos(j3_result.position)
       set_j3_pos(j3_pos)
@@ -163,7 +177,10 @@ export default function Home() {
 
       const j4_result = getposq(j4_object)
       const j4_wkq = new THREE.Quaternion();
-      j4_wkq.multiplyQuaternions(j4_object.quaternion,j3_wkq).normalize()
+//      j4_wkq.multiplyQuaternions(j4_object.quaternion,j3_wkq).normalize()
+      j4_wkq.multiplyQuaternions(j3_wkq,j4_object.quaternion).normalize()
+      set_j4_q({x:j4_wkq.x,y:j4_wkq.y,z:j4_wkq.z,w:j4_wkq.w})
+
       const j4_rot = getquaternion_Euler(j4_wkq)
       const j4_pos = getpos(j4_result.position)
       set_j4_pos(j4_pos)
@@ -171,7 +188,10 @@ export default function Home() {
 
       const j5_result = getposq(j5_object)
       const j5_wkq = new THREE.Quaternion();
-      j5_wkq.multiplyQuaternions(j5_object.quaternion,j4_wkq).normalize()
+//      j5_wkq.multiplyQuaternions(j5_object.quaternion,j4_wkq).normalize()
+      j5_wkq.multiplyQuaternions(j4_wkq,j5_object.quaternion).normalize()
+      set_j5_q({x:j5_wkq.x,y:j5_wkq.y,z:j5_wkq.z,w:j5_wkq.w})
+      
       const j5_rot = getquaternion_Euler(j5_wkq)
       const j5_pos = getpos(j5_result.position)
       set_j5_pos(j5_pos)
@@ -179,7 +199,10 @@ export default function Home() {
 
       const j6_result = getposq(j6_object)
       const j6_wkq = new THREE.Quaternion();
-      j6_wkq.multiplyQuaternions(j6_object.quaternion,j5_wkq).normalize()
+//      j6_wkq.multiplyQuaternions(j6_object.quaternion,j5_wkq).normalize()
+      j6_wkq.multiplyQuaternions(j5_wkq,j6_object.quaternion).normalize()
+      set_j6_q({x:j6_wkq.x,y:j6_wkq.y,z:j6_wkq.z,w:j6_wkq.w})
+
       const j6_rot = getquaternion_Euler(j6_wkq)
       const j6_pos = getpos(j6_result.position)
       //  console.log(`Q x:${result.rot.x} y:${result.rot.y} z:${result.rot.z}`)
@@ -205,6 +228,19 @@ export default function Home() {
             });
           }
         });
+        AFRAME.registerComponent('setq', {
+          schema: {type: 'vec4', default: { x:0 , y:0,z:0,w:1}}
+          ,
+          init: function () {
+              this.el.object3D.quaternion.set(this.data.x,this.data.y,this.data.z,this.data.w)
+          },
+          update: function () {
+            this.el.object3D.quaternion.set(this.data.x,this.data.y,this.data.z,this.data.w)
+          }
+        });
+
+
+
         AFRAME.registerComponent('j1', {
           init: function () {
             set_j1_object(this.el.object3D)
@@ -269,20 +305,29 @@ export default function Home() {
           <a-camera id="camera" cursor="rayOrigin: mouse;" position="0 0 0"></a-camera>
         </a-entity>
         <a-sphere position={edit_pos(target)} scale="0.012 0.012 0.012" color="yellow" visible={true}></a-sphere>
-        <Cursor3dp id="cj1" pos={j1_pos} rot={j1_rot} visible={true}></Cursor3dp>
-        <Cursor3dp pos={j2_pos} rot={j2_rot} visible={true}></Cursor3dp>
-        <Cursor3dp pos={j3_pos} rot={j3_rot} visible={true}></Cursor3dp>
-        <Cursor3dp pos={j4_pos} rot={j4_rot} visible={true}></Cursor3dp>
-        <Cursor3dp pos={j5_pos} rot={j5_rot} visible={true}></Cursor3dp>
-        <Cursor3dp pos={j6_pos} rot={j6_rot} visible={true}></Cursor3dp>
+        <Cursor3dp id="cj1" pos={j1_pos} rot={j1_rot} visible={false}></Cursor3dp>
+        <Cursor3dpQ id="qj1" pos={j1_pos} q={j1_q} visible={true}></Cursor3dpQ>
+        <Cursor3dpQ id="qj2" pos={j2_pos} q={j2_q} visible={true}></Cursor3dpQ>
+        <Cursor3dpQ id="qj3" pos={j3_pos} q={j3_q} visible={true}></Cursor3dpQ>
+        <Cursor3dpQ id="qj4" pos={j4_pos} q={j4_q} visible={true}></Cursor3dpQ>
+        <Cursor3dpQ id="qj5" pos={j5_pos} q={j5_q} visible={true}></Cursor3dpQ>
+        <Cursor3dpQ id="qj6" pos={j6_pos} q={j6_q} visible={true}></Cursor3dpQ>
+        <Cursor3dp pos={j2_pos} rot={j2_rot} visible={false}></Cursor3dp>
+        <Cursor3dp pos={j3_pos} rot={j3_rot} visible={false}></Cursor3dp>
+        <Cursor3dp pos={j4_pos} rot={j4_rot} visible={false}></Cursor3dp>
+        <Cursor3dp pos={j5_pos} rot={j5_rot} visible={false}></Cursor3dp>
+        <Cursor3dp pos={j6_pos} rot={j6_rot} visible={false}></Cursor3dp>
         <Line pos1={j1_pos} pos2={j2_pos} visible={true}></Line>
         <Line pos1={j2_pos} pos2={j3_pos} visible={true}></Line>
         <Line pos1={j3_pos} pos2={j4_pos} visible={true}></Line>
         <Line pos1={j4_pos} pos2={j5_pos} visible={true}></Line>
         <Line pos1={j5_pos} pos2={j6_pos} visible={true}></Line>
         <Line pos1={j1_pos} pos2={j6_pos} visible={true} color="yellow"></Line>
-        <a-box color="magenta" scale="0.12 0.12 0.12" position="0 1.1 0" rotation={edit_pos(j5_rot)} visible={true}></a-box>
-        <Cursor3dp pos={{x:0,y:1.1,z:0}} rot={j5_rot} visible={true}></Cursor3dp>
+{//        <a-box color="magenta" scale="0.12 0.12 0.12" position="0 1.1 0" rotation={edit_pos(j5_rot)} visible={true}></a-box>
+//        <Cursor3dp pos={{x:0,y:1.1,z:0}} rot={j5_rot} visible={true}></Cursor3dp>
+}
+        <a-box color="magenta" scale="0.12 0.12 0.12" position="0 1.1 0" setq={`${j5_q.x} ${j5_q.y} ${j5_q.z} ${j5_q.w}`} visible={true}></a-box>
+        <Cursor3dpQ pos={{x:0,y:1.1,z:0}} q={j5_q} visible={true}></Cursor3dpQ>
       </a-scene>
       <Controller {...controllerProps}/>
     </>
@@ -380,6 +425,23 @@ const Cursor3dp = (props) => {
       line__2={line_z}
       position={`${pos.x} ${pos.y} ${pos.z}`}
       rotation={`${rot.x} ${rot.y} ${rot.z}`}
+      visible={visible}
+  >{children}</a-entity>
+}
+
+const Cursor3dpQ = (props) => {
+  const { id, pos={x:0,y:0,z:0}, q, len=0.5, opa=1, children, visible=false } = props;
+
+  const line_x = `start: 0 0 0; end: ${len} 0 0; color: olive; opacity: ${opa};`
+  const line_y = `start: 0 0 0; end: 0 ${len} 0; color: lime; opacity: ${opa};`
+  const line_z = `start: 0 0 0; end: 0 0 ${len}; color: aqua; opacity: ${opa};`
+
+  return <a-entity id={id}
+      line={line_x}
+      line__1={line_y}
+      line__2={line_z}
+      position={`${pos.x} ${pos.y} ${pos.z}`}
+      setq={`${q.x} ${q.y} ${q.z} ${q.w}`}
       visible={visible}
   >{children}</a-entity>
 }
